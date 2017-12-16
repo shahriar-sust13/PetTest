81d76e0fde98be297c8d6b0a766a2e18a5145c29
draak-krijger <nazimsust@gmail.com>
2017-12-16 17:08:17 +0600

    model create

---
+++ app/petImage.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class petImage extends Model
+{
+    //
+    protected $table = 'petimages';
+}
---
+++ app/userProfile.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class userProfile extends Model
+{
+    //
+    protected $table = 'userprofile';
+}
